c494c555f445f11d406c1d884b12f523f97bf174
Elmer Jose Diaz Lazo <elmerdiazlazo@gmail.com>
2013-10-06 10:58:40 -0500

    guardado antes de adicionar el modulo de bodega

---
+++ src/main/java/co/com/rentavoz/logica/jpa/entidades/caja/EstadoCaja.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package co.com.rentavoz.logica.jpa.entidades.caja;
+
+/**
+ * @author <a href="mailto:elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+ * @project co.com.rentavoz.model.jpa
+ * @class EstadoCaja
+ * @date 3/10/2013
+ *
+ */
+public enum EstadoCaja {
+ABIERTA,CERRADA
+}
---
+++ src/main/java/co/com/rentavoz/logica/jpa/fachadas/CajaFacade.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.rentavoz.logica.jpa.fachadas;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import co.com.rentavoz.logica.jpa.entidades.caja.Caja;
+
+@Stateless
+public class CajaFacade extends AbstractFacade<Caja> {
+	@PersistenceContext(unitName = "com.innovate.rentavozPU")
+	private EntityManager em;
+
+	@Override
+	protected EntityManager getEntityManager() {
+		return em;
+	}
+
+	public CajaFacade() {
+		super(Caja.class);
+	}
+
+}
